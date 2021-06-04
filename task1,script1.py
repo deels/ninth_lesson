f627daddba66efa95c6fc290ef2ab434b0f1f3df
Azarov Oleg <74871854+deels@users.noreply.github.com>
2021-06-04 22:36:09 +0300

    Update task1,script1.py

--- task1,script1.py
+++ task1,script1.py
@@ -1,3 +1,9 @@
+# TODO: Task 1
+#  Files
+#  Write a script that creates a new output file called myfile.txt and writes the string "Hello file world!" in it.
+#  Then write another script that opens myfile.txt, and reads and prints its contents. Run your two scripts from the
+#  system command line.
+
 text = 'Hello file world'
 with open('myfile.txt', 'w') as mf:
     mf.write(text)
